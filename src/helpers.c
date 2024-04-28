d3e074c47f3ac75748dcbf628483997d6ba4e0c1
Giovanni Dueck <giodueck@gmail.com>
2024-04-28 19:16:29 -0400

    Add base64 decoding function

--- src/helpers.c
+++ src/helpers.c
@@ -119,17 +119,64 @@ char *str_replace(char *haystack, size_t haystacksize, const char *oldneedle, co
 }
 
 // Lookup table for 6-bit keys
-static const char base64_encode_lookup[64] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+static const char base64_to_ascii[65] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+// Lookup table for 8-bit ascii keys
+static const int ascii_to_base64[256] =
+{
+	/* ASCII table */
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 62, 64, 64, 64, 63,
+	52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 64, 64, 64, 64, 64, 64,
+	64,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 64, 64, 64, 64, 64,
+	64, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64,
+	64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64, 64
 };
 
 // Base64 decode
+// Quirks: requires padding, allows no characters outside of the base64 alphabet, not even whitespace.
+// On successful decoding returns 0, if an error was encountered with the data, -1.
 // decoded: destination buffer for decoded bytes data. Assumed to be big enough, does not null-terminate.
 // b64_data: null-terminated string of base 64 data to decode.
-int base64_decode(char *decoded, char *b64_data);
+int base64_decode(char *decoded, char *b64_data)
+{
+    int j = 0;
+    int qi = 0;
+    char quad[4] = { 0 };
+    int pad = 0;
+
+    for (int i = 0; b64_data[i] != '\0' && pad == 0; i++)
+    {
+        if (ascii_to_base64[b64_data[i]] == 64)
+            return -1;
+
+        quad[qi++] = ascii_to_base64[b64_data[i]];
+        pad += (b64_data[i] == '=');
+
+        if (qi == 4)
+        {
+            decoded[j++] = quad[0] << 2 | quad[1] >> 4;
+            if (pad < 2) decoded[j++] = (quad[1] & 0xF) << 4 | quad[2] >> 2;
+            if (pad < 1) decoded[j++] = (quad[2] & 0x3) << 6 | quad[3];
+            qi = 0;
+
+            if (pad) break;
+        }
+    }
+
+    return 0;
+}
 
 // Base64 encode
 // encoded: destination buffer for encoded base 64 null-terminated string. Assumed to be big enough.
@@ -149,10 +196,10 @@ int base64_encode(char *encoded, char *bytes_data, size_t bytes_len)
         triplet[ti++] = bytes_data[i];
         if (ti == 3)
         {
-            encoded[j++] = base64_encode_lookup[triplet[0] >> 2];
-            encoded[j++] = base64_encode_lookup[(triplet[0] & 0x3) << 4 | (triplet[1] >> 4)];
-            encoded[j++] = base64_encode_lookup[(triplet[1] & 0xF) << 2 | (triplet[2] >> 6)];
-            encoded[j++] = base64_encode_lookup[triplet[2] & 0x3F];
+            encoded[j++] = base64_to_ascii[triplet[0] >> 2];
+            encoded[j++] = base64_to_ascii[(triplet[0] & 0x3) << 4 | (triplet[1] >> 4)];
+            encoded[j++] = base64_to_ascii[(triplet[1] & 0xF) << 2 | (triplet[2] >> 6)];
+            encoded[j++] = base64_to_ascii[triplet[2] & 0x3F];
             ti = 0;
         }
     }
@@ -161,15 +208,15 @@ int base64_encode(char *encoded, char *bytes_data, size_t bytes_len)
     switch (ti)
     {
         case 1:
-            encoded[j++] = base64_encode_lookup[triplet[0] >> 2];
-            encoded[j++] = base64_encode_lookup[(triplet[0] & 0x3) << 4];
+            encoded[j++] = base64_to_ascii[triplet[0] >> 2];
+            encoded[j++] = base64_to_ascii[(triplet[0] & 0x3) << 4];
             encoded[j++] = '=';
             encoded[j++] = '=';
             break;
         case 2:
-            encoded[j++] = base64_encode_lookup[triplet[0] >> 2];
-            encoded[j++] = base64_encode_lookup[(triplet[0] & 0x3) << 4 | (triplet[1] >> 4)];
-            encoded[j++] = base64_encode_lookup[(triplet[1] & 0xF) << 2];
+            encoded[j++] = base64_to_ascii[triplet[0] >> 2];
+            encoded[j++] = base64_to_ascii[(triplet[0] & 0x3) << 4 | (triplet[1] >> 4)];
+            encoded[j++] = base64_to_ascii[(triplet[1] & 0xF) << 2];
             encoded[j++] = '=';
             break;
         default:
